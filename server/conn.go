f91964643a1e208012fd8ba1b90704bfeafc73e8
lilei1 <lileiaab@digitalchina.com>
2021-04-22 15:01:08 +0800

    错误信息转换

--- server/conn.go
+++ server/conn.go
@@ -1094,32 +1094,26 @@ func (cc *clientConn) writeError(ctx context.Context, e error) error {
 	//读包获取sql，去除第一位的类型
 	sql := string(cc.lastPacket)[1 : ]
 
-	//Get the file location and line number where the error occurred
-	var filePath string
-	var line int
-	if te != nil {
-		filePath, line = te.Location()
-	}
-	code, pgMsg, position, err := convertMysqlErrorToPgError(m, sql)
+	errorResponse, err := convertMysqlErrorToPgError(m, te, sql)
 	if err != nil {
 		return err
 	}
 	cc.lastCode = m.Code
 	// todo 完成MySQL错误与PgSQL错误的转换和返回
 	// https://www.postgresql.org/docs/13/errcodes-appendix.html
-	errorResponse := pgproto3.ErrorResponse{
-		Severity:            "ERROR",
-		SeverityUnlocalized: "",
-		Code:                code,
-		Message:             pgMsg,
-		Detail:              "",
-		Hint:                "",
-		Position:            position,
-		// the file name of the source-code location where the error was reported.
-		Line:                int32(line),
-		// the line number of the source-code location where the error was reported.
-		File:				filePath,
-	}
+	//errorResponse := pgproto3.ErrorResponse{
+	//	Severity:            "ERROR",
+	//	SeverityUnlocalized: "",
+	//	Code:                code,
+	//	Message:             pgMsg,
+	//	Detail:              "",
+	//	Hint:                "For example, FROM (SELECT ...) [AS] foo.",
+	//	Position:            position,
+	//	// the file name of the source-code location where the error was reported.
+	//	Line:                int32(line),
+	//	// the line number of the source-code location where the error was reported.
+	//	File:				filePath,
+	//}
 
 	if err := cc.WriteData(errorResponse.Encode(nil)); err != nil {
 		return err
@@ -2489,30 +2483,303 @@ func loadSSLCertificates()(tlsConfig *tls.Config,err error){
 	return tlsConfig,nil
 }
 // convertMysqlErrorToPgError 从mysql消息体中获取信息转换出pgsql所需要的信息
-func convertMysqlErrorToPgError(m *mysql.SQLError, sql string) (code, pgMsg string, position int32, err error) {
-	defaultCode,defaultMsg := "XX000","unknown error"
+func convertMysqlErrorToPgError(m *mysql.SQLError, te *terror.Error, sql string) (response *pgproto3.ErrorResponse, err error) {
 	switch m.Code {
+	case 1007:
+		return handleCreateDBFail(m,te,sql)
+	case 1008:
+		return handleDropDBFail(m,te,sql)
+	case 1045:
+		return handleAccessDenied(m,te,sql)
+	case 1050:
+		return handleTableExists(m,te,sql)
 	case 1054:
-		return handleUnknownColumn(m,sql)
+		return handleUnknownColumn(m,te, sql)
 	case 1062:
-		return handleDuplicateKey(m)
+		return handleDuplicateKey(m,te,sql)
 	case 1064:
-		return handleParseError(m, sql)
-		//type error
+		return handleParseError(m,te, sql)
 	case 1105:
-		return handleTypeError(m, sql)
+		return handleTypeError(m, te,sql)
 	case 1136:
-		return handeleColumnMisMatch(m,sql)
+		return handeleColumnMisMatch(m, te,sql)
+	case 1142:
+		return handleTableAccessDenied(m,te, sql)
+	case 1143:
+		return handleColumnAccessDenied(m,te, sql)
 	case 1146:
-		return handleRelationNotExists(m, sql)
+		return handleRelationNotExists(m, te,sql)
+	case 1211:
+		return handleNoPermissionToCreateUser(m,te, sql)
+	case 1242:
+		return handleSubqueryNo1Row(m ,te,sql)
+	case 1248:
+		return handleDerivedMustHaveAlias(m,te, sql)
+	case 1364:
+		return handleNoDefaultValue(m,te, sql)
 	default:
-		return defaultCode, defaultMsg, position, err
+		return &pgproto3.ErrorResponse{
+			Code: "XX0000",
+			Severity: "ERROR",
+			Message: "Unknown Error",
+		}, err
 	}
 }
 
-//handeleColumnMisMatch
-func handeleColumnMisMatch(m *mysql.SQLError, sql string) (string, string, int32, error) {
+//handleDerivedMustHaveAlias 处理派生表缺少别名的错误
+// eg. Every derived table must have its own alias
+func handleDerivedMustHaveAlias(m *mysql.SQLError, te *terror.Error, sql string) (*pgproto3.ErrorResponse, error) {
+	brackets := "("
+
+	errorResp := &pgproto3.ErrorResponse{
+		Severity: "ERROR",
+		SeverityUnlocalized: "",
+		Code: "tobe",
+		Detail: "",
+		Hint: "",
+	}
+	setFilePathAndLine(te, errorResp)
+
+	pgMsg := "subquery in FROM must have an alias"
+
+	position := strings.Index(sql, brackets) + 3
+
+	errorResp.Message, errorResp.Position = pgMsg, int32(position)
+
+	return errorResp, nil
+}
+
+//handleSubqueryNo1Row 处理子查询返回数据行数超过一行的错误
+//eg. Subquery returns more than 1 row
+func handleSubqueryNo1Row(m *mysql.SQLError, te *terror.Error, sql string) (*pgproto3.ErrorResponse, error) {
+
+	errorResp := &pgproto3.ErrorResponse{
+		Severity: "ERROR",
+		SeverityUnlocalized: "",
+		Code: "tobe",
+		Detail: "",
+		Hint: "",
+	}
+	setFilePathAndLine(te, errorResp)
+
+	pgMsg := "more than one row returned by a subquery used as an expression"
+	errorResp.Message =  pgMsg
+
+	return errorResp, nil
+}
+
+//handleNoPermissionToCreateUser 处理无权创建用户的错误信息
+func handleNoPermissionToCreateUser(m *mysql.SQLError, te *terror.Error, sql string) (*pgproto3.ErrorResponse, error) {
+	//eg. 'u'@'192.168.2.111' is not allowed to create new users
+
+	errorResp := &pgproto3.ErrorResponse{
+		Severity: "ERROR",
+		SeverityUnlocalized: "",
+		Code: "tobe",
+		Detail: "",
+		Hint: "",
+	}
+	setFilePathAndLine(te, errorResp)
+
+	pgMsg := "permission denied to create role"
+	errorResp.Message = pgMsg
+
+	return errorResp, nil
+}
+
+//handleTableAccessDenied 处理数据表访问被拒的错误信息 todo 有待权限完善后测试
+func handleTableAccessDenied(m *mysql.SQLError, te *terror.Error, sql string) (*pgproto3.ErrorResponse, error) {
+	//eg. SELECT command denied to user 'XXX'@'localhost' for table 'db'
+	msg, beforeTable, apostrophe := m.Message, "table ", "'"
+
+	errorResp := &pgproto3.ErrorResponse{
+		Severity: "ERROR",
+		SeverityUnlocalized: "",
+		Code: "2F004",
+		Detail: "",
+		Hint: "",
+	}
+	setFilePathAndLine(te,errorResp)
+
+	tableStart := strings.Index(msg, beforeTable) + len(beforeTable)
+	tableLen := strings.Index(msg, apostrophe)
+	table := strings.Trim(msg[tableStart : tableStart + tableLen], apostrophe)
+
+	pgMsg := fmt.Sprintf("permission denied for table %s", table)
+	errorResp.Message = pgMsg
+
+	return errorResp, nil
+}
+
+//handleColumnAccessDenied 处理字段访问拒绝信息 todo 写这里的时候dctidb权限模块还没做好，所以没有测试，后续需要测试一下。
+// pgsql权限粒度没有达到字段级别，所以pg的错误信息中不会指出哪个字段访问被拒,只会指明表访问被拒，这里与pgsql保持一致、
+func handleColumnAccessDenied(m *mysql.SQLError, te *terror.Error, sql string) (*pgproto3.ErrorResponse, error) {
+	// eg. SELECT command denied to user 'e1534'@'192.168.176.100' for column 'id' in table 't1'
+	msg, beforeTable, apostrophe := m.Message, "table ", "'"
+
+	errorResp := &pgproto3.ErrorResponse{
+		Severity: "ERROR",
+		SeverityUnlocalized: "",
+		Code: "2F004",
+		Detail: "",
+		Hint: "",
+	}
+	setFilePathAndLine(te, errorResp)
+
+	tableStart := strings.Index(msg, beforeTable) + len(beforeTable)
+	tableLen := strings.Index(msg, apostrophe)
+	table := strings.Trim(msg[tableStart : tableStart + tableLen], apostrophe)
+
+	pgMsg := fmt.Sprintf("permission denied for table %s", table)
+
+	errorResp.Message = pgMsg
+
+	return errorResp, nil
+}
+
+//handleNoDefaultValue 处理非空字段缺少默认值的情况
+func handleNoDefaultValue(m *mysql.SQLError, te *terror.Error, sql string) (*pgproto3.ErrorResponse, error) {
+	msg, beforeColumn, empty, apostrophe, into, brackets := m.Message, "Field ", " ", "'", "INTO ", "("
+
+	errorResp := &pgproto3.ErrorResponse{
+		Severity: "ERROR",
+		SeverityUnlocalized: "",
+		Code: "tobe",
+		Detail: "",
+		Hint: "",
+	}
+	setFilePathAndLine(te, errorResp)
+
+	columnStart := strings.Index(msg, beforeColumn) + len(beforeColumn)
+	columnLen := strings.Index(msg[columnStart : ], empty)
+	column := strings.Trim(msg[columnStart : columnStart + columnLen], apostrophe)
+
+	relationStart := strings.Index(strings.ToUpper(sql), into) + len(into)
+	relationLen := strings.Index(sql[relationStart : ], empty)
+	bracketsLen := strings.Index(sql[relationStart : ], brackets)
+
+	var relation string
+	if bracketsLen < relationLen && bracketsLen != -1 {
+		relation = sql[relationStart : relationStart + bracketsLen]
+	}else {
+		relation = sql[relationStart : relationStart + relationLen]
+	}
+	//原版pg对应错误还有一行错误信息，eg.描述:  Failing row contains (null, xxx    , null).，但是MySQL的错误信息不足以凑出第二行的信息。
+	pgMsg := fmt.Sprintf("null value in column \"%s\" of relation \"%s\" violates not-null constraint", column, relation)
+
+	errorResp.Message = pgMsg
+
+	return errorResp, nil
+}
+
+//handleAccessDenied 处理登录失败信息 todo 写这里的时候dctidb没有密码，也没法测试，需要加上密码之后测试
+func handleAccessDenied(m *mysql.SQLError, te *terror.Error, sql string) (*pgproto3.ErrorResponse, error) {
+	//eg.Access denied for user 'root'@'localhost' (using password: YES)
+	msg, beforeUser, at, apostrophe := m.Message, "user ", "@", "'"
+
+	errorResp := &pgproto3.ErrorResponse{
+		Severity: "ERROR",
+		SeverityUnlocalized: "",
+		Code: "tobe",
+		Detail: "",
+		Hint: "",
+	}
+	setFilePathAndLine(te, errorResp)
+
+	userStart := strings.Index(msg, beforeUser) + len(beforeUser)
+	userLen := strings.Index(msg[userStart : ], at)
+	user := strings.Trim(msg[userStart : userStart + userLen], apostrophe)
+
+	pgMsg := fmt.Sprintf("password authentication failed for user \"%s\"",user)
+
+	errorResp.Message = pgMsg
+
+	return errorResp,nil
+}
+
+//handleDropDBFail 处理数据库不存在，删除数据库失败的错误信息
+func handleDropDBFail(m *mysql.SQLError, te *terror.Error, sql string) (*pgproto3.ErrorResponse, error) {
+
+	msg, beforeDatabase,semicolon, apostrophe := m.Message, "database ", ";", "'"
+
+	errorResponse := &pgproto3.ErrorResponse{
+		Severity: "ERROR",
+		SeverityUnlocalized: "",
+		Code: "tobe",
+		Detail: "",
+		Hint: "",
+	}
+	setFilePathAndLine(te,errorResponse)
+
+	databaseStart := strings.Index(msg, beforeDatabase) + len(beforeDatabase)
+	databaseLen := strings.Index(msg[databaseStart : ], semicolon)
+	database := strings.Trim(msg[databaseStart : databaseStart + databaseLen], apostrophe)
+
+	pgMsg := fmt.Sprintf("database \"%s\" does not exist", database)
+
+	errorResponse.Message = pgMsg
+
+	return errorResponse, nil
+}
+
+//handleCreateDBFail 数据库已存在，创建数据库失败
+func handleCreateDBFail(m *mysql.SQLError, te *terror.Error, sql string) (*pgproto3.ErrorResponse, error) {
+	msg, beforeDatabase,semicolon, apostrophe := m.Message, "database ", ";", "'"
+
+	errorResponse := &pgproto3.ErrorResponse{
+		Severity: "ERROR",
+		SeverityUnlocalized: "",
+		Code: "tobe",
+		Detail: "",
+		Hint: "",
+	}
+	setFilePathAndLine(te, errorResponse)
+
+	databaesStart := strings.Index(msg, beforeDatabase) + len(beforeDatabase)
+	databaseLen := strings.Index(msg[databaesStart : ], semicolon)
+	database := strings.Trim(msg[databaesStart : databaesStart + databaseLen], apostrophe)
+	pgMsg := fmt.Sprintf("database \"%s\" already exists", database)
+
+	errorResponse.Message = pgMsg
+
+	return errorResponse, nil
+}
+
+//handleTableExists 处理表已经存在，重复创建表的错误信息
+func handleTableExists(m *mysql.SQLError, te *terror.Error, sql string) (*pgproto3.ErrorResponse, error) {
+	msg, point, apostrophe := m.Message, ".", "'"
+
+	errorResponse := &pgproto3.ErrorResponse{
+		Severity: "ERROR",
+		SeverityUnlocalized: "",
+		Code: "tobe",
+		Detail: "",
+		Hint: "",
+	}
+	setFilePathAndLine(te, errorResponse)
+
+	relationStart := strings.Index(msg, point) + 1
+	relationLen := strings.Index(msg[relationStart : ], apostrophe)
+	relation := msg[relationStart : relationStart + relationLen]
+	pgMsg := fmt.Sprintf("relation \"%s\" already exists", relation)
+
+	errorResponse.Message = pgMsg
+
+	return errorResponse, nil
+}
+
+//handeleColumnMisMatch 处理字段与值的数量匹配不上的情况
+func handeleColumnMisMatch(m *mysql.SQLError, te *terror.Error, sql string) (*pgproto3.ErrorResponse, error) {
 	values := "values"
+
+	errorResp := &pgproto3.ErrorResponse{
+		Severity: "ERROR",
+		SeverityUnlocalized: "",
+		Code: "tobe",
+		Detail: "",
+		Hint: "",
+	}
+	setFilePathAndLine(te, errorResp)
 
 	valueStart := strings.Index(sql, values) + len(values)
 	//这里的游标只能指到values后面的括号，mysql的错误信息里拿不到哪些 value 是多出来的。
@@ -2520,14 +2787,25 @@ func handeleColumnMisMatch(m *mysql.SQLError, sql string) (string, string, int32
 
 	pgMsg := fmt.Sprintf("INSERT has more expressions than target columns")
 
-	return "", pgMsg, int32(position), nil
+	errorResp.Message, errorResp.Position = pgMsg, int32(position)
+
+	return errorResp, nil
 }
 
-//handleUnknownColumn
-func handleUnknownColumn(m *mysql.SQLError, sql string) (string, string, int32, error) {
+//handleUnknownColumn 处理未知字段名的错误信息
+func handleUnknownColumn(m *mysql.SQLError, te *terror.Error, sql string) (*pgproto3.ErrorResponse, error) {
 	msg, column, into, apostrophe, bracket,empty := m.Message, "column ", "into ","'", "(", " "
 
 	sel, del, insert, update := "SELECT","DELETE","INSERT","UPDATE"
+
+	errorResp := &pgproto3.ErrorResponse{
+		Severity: "ERROR",
+		SeverityUnlocalized: "",
+		Code: "tobe",
+		Detail: "",
+		Hint: "",
+	}
+	setFilePathAndLine(te, errorResp)
 
 	columnNameStart := strings.Index(msg, column) + len(column)
 	columnNameLen := strings.Index(msg[columnNameStart : ], empty) + 1
@@ -2548,19 +2826,32 @@ func handleUnknownColumn(m *mysql.SQLError, sql string) (string, string, int32, 
 	} else if strings.HasPrefix(strings.ToUpper(strings.Trim(sql, empty)), del) {
 		pgMsg = fmt.Sprintf("column \"%s\" does not exist", columnName)
 	} else if strings.HasPrefix(strings.ToUpper(strings.Trim(sql, empty)), sel) {
-
+		pgMsg = fmt.Sprintf("column \"%s\" does not exist", columnName)
 	} else if strings.HasPrefix(strings.ToUpper(strings.Trim(sql, empty)), update) {
-
+		relationStart := strings.Index(strings.ToUpper(sql), update)
+		relationLen := strings.Index(strings.Trim(sql[relationStart : ], empty), empty)
+		relation = sql[relationStart : relationStart + relationLen]
+		pgMsg = fmt.Sprintf("column \"%s\" of relation \"%s\" does not exist", columnName, relation)
 	}
 
 	position := strings.Index(sql, columnName) + 3
+	errorResp.Message, errorResp.Position = pgMsg, int32(position)
 
-	return "tobe", pgMsg, int32(position), nil
+	return errorResp, nil
 }
 
-// handleDuplicateKey
-func handleDuplicateKey(m *mysql.SQLError) (string, string, int32, error) {
+// handleDuplicateKey 处理键冲突的问题
+func handleDuplicateKey(m *mysql.SQLError, te *terror.Error, sql string) (*pgproto3.ErrorResponse, error) {
 	msg, entry, key, apostrophe, empty := m.Message,"entry ", "key ", "'", " "
+
+	errorResp := &pgproto3.ErrorResponse{
+		Severity: "ERROR",
+		SeverityUnlocalized: "",
+		Code: "tobe",
+		Detail: "",
+		Hint: "",
+	}
+	setFilePathAndLine(te, errorResp)
 
 	keyStart := strings.Index(msg, key) + len(key) + 1
 	keyName := strings.Trim(msg[keyStart : ], apostrophe)
@@ -2570,27 +2861,46 @@ func handleDuplicateKey(m *mysql.SQLError) (string, string, int32, error) {
 	value := msg[valueStart : valueStart + valueLen]
 
 	pgMsg := fmt.Sprintf("duplicate key value violates unique constraint \"%s\"\nDescription:  Key value %v already exists.", keyName, value)
+	errorResp.Message = pgMsg
 
-	return "tobe",pgMsg,-1,nil
+	return errorResp, nil
 }
 
-// handleRelationNotExists
-func handleRelationNotExists(m *mysql.SQLError, sql string) (string, string, int32, error) {
+// handleRelationNotExists 处理表（关系）不存在的错误信息
+func handleRelationNotExists(m *mysql.SQLError, te *terror.Error, sql string) (*pgproto3.ErrorResponse, error) {
 	msg, point, apostrophe := m.Message, ".", "'"
+
+	errorResp := &pgproto3.ErrorResponse{
+		Severity: "ERROR",
+		SeverityUnlocalized: "",
+		Code: "tobe",
+		Detail: "",
+		Hint: "",
+	}
+	setFilePathAndLine(te, errorResp)
 
 	tableNameStart := strings.Index(msg,point) + 1
 	nameLen := strings.Index(msg[tableNameStart : ], apostrophe)
 	tableName := msg[tableNameStart : tableNameStart + nameLen]
-
 	pgMsg := fmt.Sprintf("relation \"%s\" does not exist", tableName)
 
 	position := strings.Index(sql, tableName) + 3
+	errorResp.Message, errorResp.Position = pgMsg, int32(position)
 
-	return "tobe", pgMsg, int32(position),nil
+	return errorResp,nil
 }
-// handleTypeError
-func handleTypeError(m *mysql.SQLError, sql string) (string, string, int32, error) {
+// handleTypeError 处理类型转换错误信息
+func handleTypeError(m *mysql.SQLError, te *terror.Error, sql string) (*pgproto3.ErrorResponse, error) {
 	msg, quotes, beforeInput := m.Message, "\"", "parsing "
+
+	errorResp := &pgproto3.ErrorResponse{
+		Severity: "ERROR",
+		SeverityUnlocalized: "",
+		Code: "tobe",
+		Detail: "",
+		Hint: "",
+	}
+	setFilePathAndLine(te, errorResp)
 
 	inputStart := strings.Index(msg, beforeInput) + len(beforeInput)
 	inputLen := strings.Index(msg[inputStart + 1 : ], quotes) + 1
@@ -2601,14 +2911,25 @@ func handleTypeError(m *mysql.SQLError, sql string) (string, string, int32, erro
 	pgMsg := fmt.Sprintf("invalid input syntax for : %s", input)
 
 	position := strings.Index(sql, input)
+	errorResp.Message, errorResp.Position = pgMsg, int32(position)
 
-	return "tobe",pgMsg,int32(position),nil
+	return errorResp,nil
 }
 
-// handleParseError
-func handleParseError(m *mysql.SQLError, sql string) (string, string, int32, error) {
-	msg, nearDelimiter, positionDelimiter, wrap, quotes, emptyStr := m.Message, "near ", "column ", "\n","\""," "
-	// eg. You have an error in your SQL syntax; check the manual that corresponds to your TiDB version for the right syntax to use line 1 column 5 near "creat table student(id int);"
+// handleParseError 处理编译sql出错的信息
+// eg. You have an error in your SQL syntax; check the manual that corresponds to your TiDB version for the right syntax to use line 1 column 5 near "creat table student(id int);"
+func handleParseError(m *mysql.SQLError, te *terror.Error, sql string) (*pgproto3.ErrorResponse, error) {
+	msg, nearDelimiter, wrap, quotes, emptyStr := m.Message, "near ", "\n","\""," "
+
+	errorResp := &pgproto3.ErrorResponse{
+		Severity: "ERROR",
+		SeverityUnlocalized: "",
+		Code: "42601",
+		Detail: "",
+		Hint: "",
+	}
+	setFilePathAndLine(te, errorResp)
+
 	nearStart := strings.Index(msg, nearDelimiter) + len(nearDelimiter)
 	nearlen := strings.Index(strings.Trim(msg[nearStart : ], emptyStr), emptyStr)
 	lineEnd := strings.Index(strings.Trim(msg[nearStart : ], emptyStr), wrap)
@@ -2632,12 +2953,22 @@ func handleParseError(m *mysql.SQLError, sql string) (string, string, int32, err
 		behindNear += quotes
 	}
 
-	positionStart := strings.Index(msg, positionDelimiter) + len(positionDelimiter)
-	positionLen := strings.Index(strings.Trim(msg[positionStart : ], emptyStr), emptyStr)
-	pos, err := strconv.Atoi(msg[positionStart : positionStart + positionLen])
+	pgMsg := fmt.Sprintf("syntax error at or near %s",behindNear)
 
 	// 为什么加3? 在提示信息第二行开头回提示是第几行sql的错误，他也会占偏移量，所以要将其加上。
-	pos = strings.Index(sql,strings.Trim(behindNear, quotes)) + 3
+	position := strings.Index(sql,strings.Trim(behindNear, quotes)) + 3
+	errorResp.Message, errorResp.Position = pgMsg, int32(position)
 
-	return "42601", fmt.Sprintf("syntax error at or near %s",behindNear), int32(pos), err
+	return errorResp, nil
+}
+
+//setFilePathAndLine 通过terror.Error对象获取出错文件和行位置
+func setFilePathAndLine(te *terror.Error, errorResponse *pgproto3.ErrorResponse){
+	//Get the file location and line number where the error occurred
+	var filePath string
+	var line int
+	if te != nil {
+		filePath, line = te.Location()
+	}
+	errorResponse.File, errorResponse.Line = filePath, int32(line)
 }
