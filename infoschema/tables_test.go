14d6430ba40325d051ed3fb7d2b98aa7e535f044
Orion7r <50295175+Orion7r@users.noreply.github.com>
2021-08-13 10:31:55 +0800

    style: fmt check
    Signed-off-by: Orion7r <50295175+Orion7r@users.noreply.github.com>

--- infoschema/tables_test.go
+++ infoschema/tables_test.go
@@ -981,9 +981,9 @@ func (s *testTableSuite) TestStmtSummaryTable(c *C) {
 		from information_schema.statements_summary
 		where digest_text like 'select * from t%'`,
 	).Check(testkit.Rows("Select test test.t t:k 1 2 0 0 0 0 0 0 0 0 0 select * from t where a=2 \tid                \ttask     \testRows\toperator info\n" +
-		 "\tIndexLookUp_10    \troot     \t100     \t\n" +
-	     "\t├─IndexRangeScan_8\tcop[tikv]\t100     \ttable:t, index:k(a), range:[2,2], keep order:false, stats:pseudo\n" +
-	     "\t└─TableRowIDScan_9\tcop[tikv]\t100     \ttable:t, keep order:false, stats:pseudo"))
+		"\tIndexLookUp_10    \troot     \t100    \t\n" +
+		"\t├─IndexRangeScan_8\tcop[tikv]\t100    \ttable:t, index:k(a), range:[2,2], keep order:false, stats:pseudo\n" +
+		"\t└─TableRowIDScan_9\tcop[tikv]\t100    \ttable:t, keep order:false, stats:pseudo"))
 
 	// select ... order by
 	tk.MustQuery(`select stmt_type, schema_name, table_names, index_names, exec_count, sum_cop_task_num, avg_total_keys,
